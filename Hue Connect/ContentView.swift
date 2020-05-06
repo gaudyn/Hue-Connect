d9255100c5280443743342f2772e9b9f63ac230d
gniewko717 <ggaudyn00@gmail.com>
2020-05-06 19:45:19 +0200

    Testing game view

--- Hue Connect/ContentView.swift
+++ Hue Connect/ContentView.swift
@@ -77,13 +77,44 @@ struct NavigationButtons: View{
 }
 
 struct ContentView: View {
+    @State private var timeLeft: Double = 100
+    var timeTimer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            
+            VStack {
+                CustomSlider(value: $timeLeft, range: (0, 100), knobWidth: 0) { (modifiers) in
+                    ZStack{
+                        LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow, Color.green]), startPoint: .leading, endPoint: .trailing)
+                        Color(UIColor.systemBackground).modifier(modifiers.barRight)
+                    }
+                }
+                .frame(height: 4)
+                .onReceive(timeTimer) { _ in
+                    if(self.timeLeft > 0){
+                        self.timeLeft -= 0.01
+                        
+                    }
+                }
+                GridStack(rows:12, columns: 16){ row, col in
+                    if(row == 0 || row == 11 || col == 0 || col == 15){
+                        TileView(colorType: .Blue, colorId: 0)
+                    }else{
+                        TileView(colorType: ColorEnum(rawValue: Int.random(in: 0...3))!, colorId: Int.random(in: 1...9))
+                    }
+                }
+                .navigationBarTitle("Hue Connect", displayMode: .large)
+                .navigationBarItems(trailing: NavigationButtons())
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(PreviewLayout.fixed(width: 2224, height: 1668))
     }
 }
