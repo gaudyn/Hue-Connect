982bbe7bd569d4314395780bee5c73e0797fd5ed
gniewko717 <ggaudyn00@gmail.com>
2020-05-11 21:31:24 +0200

    Move GridStack to different file

--- Hue Connect/View/ContentView.swift
+++ Hue Connect/View/ContentView.swift
@@ -8,30 +8,6 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View{
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View{
-        VStack(alignment: .leading, spacing: 0){
-            ForEach(0 ..< rows, id: \.self){ row in
-                HStack(spacing: 0){
-                    ForEach(0 ..< self.columns, id: \.self){ column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
-    }
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content){
-        self.rows = rows
-        self.columns = columns
-        self.content = content
-    }
-}
-
 struct NavigationButtons: View{
     
     @EnvironmentObject var board: Board
